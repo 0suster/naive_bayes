00ba23dd01dbd9391fb7e897569f785a0f442868
Rachan <tine.suster@student.um.si>
2020-01-22 18:53:45 +0100

    koncal klasificiranje

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 """
 https://www.youtube.com/watch?v=CPqOCI0ahss&t=496s
 """
-from pprint import *
 import pandas as pd
+from pprint import pprint
 
-data = pd.read_csv('data_sets/car_ucna.csv')
-data.head()
-clss = data.columns[len(data.columns) - 1]
+df_learn = pd.read_csv('data_sets/car_ucna.csv')
+dvar = df_learn.columns[len(df_learn.columns) - 1]
 """
-:var data: data-frame iz csv datoteke
-:var clss: ime stolpca kjer naj bi bil class
+:var df_learn: data-frame iz csv datoteke
+:var dvar: ime stolpca kjer naj bi bil class
 """
 
-data.sort_values(by=[clss], inplace=True)
+df_learn.sort_values(by=[dvar], inplace=True)
 col_vals = {}
-total = data.shape[0]
-cols = data.columns
+total = df_learn.shape[0]
+cols = df_learn.columns
 """
 :var col_vals: slovar, kjer so kljuci imena stolpcev data-frama, vrednosti pa arrayi vseh edinstvenih
     vrednosti tistega stolpca
@@ -23,34 +22,71 @@ cols = data.columns
 :var cols: imena vseh stolpcev
 """
 
-for item in data.columns:
-    col_vals[item] = data[item].unique()
+for item in df_learn.columns:
+    col_vals[item] = df_learn[item].unique()
 
 model = {}
 """
 :var model: slovar, kjer bodo prestete vse vrednosti vsakega atributa glede na klasifikacijo
 """
 
-for val in col_vals[clss]:
-    model[val] = {"count": (data[clss] == val).sum()}
+for val in col_vals[dvar]:
+    model[val] = {"count": (df_learn[dvar] == val).sum()}
     for col in cols:
-        if col != clss:
+        if col != dvar:
             model[val][col] = {}
 
-classifier = col_vals[clss]
+classifier = col_vals[dvar]
 """
 :var classifier = je array vseh edinstvenih vrednosti klasifikacijskega stolpca
 """
 
 
-col_vals.pop(clss)
+col_vals.pop(dvar)
 
 for cl in classifier:
     for key in col_vals.keys():
         for val in col_vals[key]:
-            sort = data.loc[data[clss] == cl]
+            sort = df_learn.loc[df_learn[dvar] == cl]
             sort1 = sort.loc[sort[key] == val]
             model[cl][key][val] = (sort1.shape[0]) / (model[cl]['count'])
 
 
+val_probs = {}
+"""
+:var val_probs: splosna verjetnost za vsako mozno vrednost v vaskem stolpcu
+"""
 
+for col in col_vals:
+    sums = df_learn[col].value_counts()
+    val_probs[col] = {}
+    for index in sums.index:
+        val_probs[col][index] = sums[index]/total
+
+
+df_test = pd.read_csv('data_sets/car_testna.csv')
+comp_class = df_test[dvar]
+"""
+:var df_test: dataframe testnega csv-ja
+:var comp_class: klasifikacije testnega data-seta pred napovedjo
+"""
+
+for key in model.keys():
+    model[key]['count'] /= total
+
+print(df_test.head(5))
+
+for index, row in df_test.iterrows():
+    select = []
+    in_prob = 1
+    for clss in classifier:
+        for col in df_test.columns:
+            if col != dvar:
+                value = row[col]
+                in_prob *= model[clss][col][value]
+
+        in_prob /= model[clss]['count']
+        select.append(in_prob)
+    for i in range(len(select)):
+        if select[i] == max(select):
+            row[dvar] = classifier[i]
--- test.py
+++ test.py
@@ -3,8 +3,3 @@ import pandas as pd
 data = pd.read_csv('data_sets/car_ucna.csv')
 data.head()
 clss = data.columns[len(data.columns) - 1]
-
-a = data.loc[data[clss] == 'good']
-b = a.loc[a['buying'] == 'high']
-
-print(data.loc[data[clss] == "acc"].shape[0])
